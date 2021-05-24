e613dddf97e0aa8f2dd0d36e4bca7639bf4ec38c
John Haupenthal <john.haupenthal@vincit.com>
2021-05-23 21:56:31 -0700

    Add navigation shell.

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -1,15 +1,25 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
-  styleUrls: ['./shell.component.scss']
+  styleUrls: ['./shell.component.scss'],
 })
 export class ShellComponent implements OnInit {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe([Breakpoints.Handset])
+    .pipe(
+      // Pull only matches off of Breakpoint change result object
+      map((result) => result.matches),
+      // Makes all subscriptions listen to same, most recent value from observable
+      // Since we will be subscribing multiple times with the async pipe
+      shareReplay()
+    );
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
