e80ae0e04b964eaa4eea819b62d95857b3fb2dc1
John Haupenthal <john.haupenthal@vincit.com>
2021-05-23 22:26:24 -0700

    Add firebase to app.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAvLjN7End4LEDfcqpUsrOZTUzL8As-0Gs',
+    authDomain: 'firestarter-demo-b2d8e.firebaseapp.com',
+    projectId: 'firestarter-demo-b2d8e',
+    storageBucket: 'firestarter-demo-b2d8e.appspot.com',
+    messagingSenderId: '820344414440',
+    appId: '1:820344414440:web:32b36f6ddc71ad7c56e95d',
+    measurementId: 'G-XFPFWB9HD8',
+  },
 };
